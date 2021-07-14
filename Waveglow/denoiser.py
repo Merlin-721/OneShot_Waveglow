14a7a33c81e4ab58e518ce976ebb813ce90bd154
ThorChadMerlin <merlinlindsay@hotmail.com>
2021-07-14 11:36:00 +0100

    removes noise from mel instead of audio

--- Waveglow/denoiser.py
+++ Waveglow/denoiser.py
@@ -1,8 +1,15 @@
 import sys
 sys.path.append('tacotron2')
 import torch
-from tacotron2.layers import STFT
+from tacotron2.layers import TacotronSTFT
+from Waveglow.mel2samp import MAX_WAV_VALUE
 
+def get_mel(audio,stft):
+    audio_norm = audio / MAX_WAV_VALUE
+    audio_norm = torch.autograd.Variable(audio_norm, requires_grad=False)
+    melspec = stft.mel_spectrogram(audio_norm)
+    melspec = torch.squeeze(melspec, 0)
+    return melspec
 
 class Denoiser(torch.nn.Module):
     """ Removes model bias from audio produced with waveglow """
@@ -10,8 +17,8 @@ class Denoiser(torch.nn.Module):
     def __init__(self, waveglow, filter_length=1024, n_overlap=4,
                  win_length=1024, mode='zeros'):
         super(Denoiser, self).__init__()
-        self.stft = STFT(filter_length=filter_length,
-                         hop_length=int(filter_length/n_overlap),
+        self.stft = TacotronSTFT(filter_length=filter_length,
+                         hop_length=int(256),
                          win_length=win_length).cuda()
         if mode == 'zeros':
             mel_input = torch.zeros(
@@ -28,13 +35,13 @@ class Denoiser(torch.nn.Module):
 
         with torch.no_grad():
             bias_audio = waveglow.infer(mel_input, sigma=0.0).float()
-            bias_spec, _ = self.stft.transform(bias_audio)
+            self.bias_spec = get_mel(audio=bias_audio,stft=self.stft)
+            # self.bias_spec = self.stft.mel_spectrogram(bias_audio)
 
-        self.register_buffer('bias_spec', bias_spec[:, :, 0][:, :, None])
+        # self.register_buffer('bias_spec', bias_spec[:, :, 0][:, :, None])
 
-    def forward(self, audio, strength=0.1):
-        audio_spec, audio_angles = self.stft.transform(audio.cuda().float())
-        audio_spec_denoised = audio_spec - self.bias_spec * strength
-        audio_spec_denoised = torch.clamp(audio_spec_denoised, 0.0)
-        audio_denoised = self.stft.inverse(audio_spec_denoised, audio_angles)
-        return audio_denoised
+    def forward(self,mel, strength=0.1):
+        noise = torch.mean(self.bias_spec, axis=1)
+        noise = torch.clamp(noise, max=0.0)
+        audio_spec_denoised = mel.T + noise * strength
+        return audio_spec_denoised.T
