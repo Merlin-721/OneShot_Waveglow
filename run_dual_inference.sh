609a2dbc3a9744c592c5587b0a78d88a04092ea0
ThorChadMerlin <merlinlindsay@hotmail.com>
2021-07-15 10:10:52 +0100

    new model paths

--- run_dual_inference.sh
+++ run_dual_inference.sh
@@ -1,13 +1,13 @@
 python dual_inference.py \
-	-source input_wavs/22k_1.wav \
-	-target input_wavs/22k_2.wav \
-	-output output_wavs/ \
-	-attr OneShot/preprocess/data/VCTK_attr.pkl \
+	-target input_wavs/p284_024.wav \
+	-source input_wavs/p341_008.wav \
+	-output output_wavs/sig_1_den_0.2_0.1_tests \
+	-output_name  blank\
+	-attr OneShot/preprocess/data/VCTK/VCTK_attr.pkl \
 	-oneshot_conf OneShot/config.yaml\
-	-oneshot_model OneShot/models/vctk_22kHz_model.ckpt\
+	-oneshot_model OneShot/models/vctk_22_kl_1_full_200k.ckpt\
 	-sample_rate 22050 \
 	-data_conf Waveglow/config.json \
 	-waveglow_path Waveglow/models/waveglow_256channels_universal_v5.pt \
-	-sigma 0.6\
-	--is_fp16 \
-	-denoiser_strength 0 \
+	-sigma 1\
+	-denoiser_strength 0.1
