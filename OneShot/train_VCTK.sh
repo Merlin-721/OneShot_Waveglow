dc3d8b72babf63e99c13cc0d548328160a105c4c
ThorChadMerlin <merlinlindsay@hotmail.com>
2021-07-15 10:04:24 +0100

    training used for model

--- OneShot/train_VCTK.sh
+++ OneShot/train_VCTK.sh
@@ -1,9 +1,9 @@
 python3 main.py \
  	-c config.yaml \
-	-d preprocess/data/VCTK/22kHz_mels \
+	-d preprocess/data/VCTK/ \
 	-train_set train_128 \
 	-train_index_file train_samples_128.json \
-	-store_model_path models/vctk_22kHz/vctk_22_kl_1 \
-	-t vctk_22_kl_1 \
+	-store_model_path models/vctk_22kHz/vctk_22_kl_1_full \
+	-t vctk_22_kl_1_full \
 	-iters 200000 \
 	-summary_step 500
