74cc521ba32b4d1756e7db4c2f719d73fae285c5
ThorChadMerlin <merlinlindsay@hotmail.com>
2021-09-02 16:47:19 +0100

    paths

--- run_dual_inference.sh
+++ run_dual_inference.sh
@@ -1,13 +1,13 @@
 python dual_inference.py \
-	-source input_wavs/p336_021.wav \
-	-target input_wavs/p284_024.wav \
-	-output output_wavs/22_dropout/sig_0.6_den_0.1 \
-	-output_name nodrop\
-	-attr OneShot/preprocess/data/VCTK/VCTK_attr.pkl \
-	-oneshot_conf OneShot/models/vctk_22kHz_dropout/vctk_22_dropout.config.yaml \
-	-oneshot_model OneShot/models/vctk_22_dropout_100k.ckpt\
-	-sample_rate 22050 \
-	-data_conf Waveglow/config.json \
-	-waveglow_path Waveglow/models/waveglow_256channels_universal_v5.pt \
-	-sigma 1\
-	-denoiser_strength 0.1
+	-source=/home/merlin/OneDrive/modules/individualProject/voiceChanger/Evaluation/Eval_Dataset_Reduced/Men \
+	-target=/home/merlin/OneDrive/modules/individualProject/voiceChanger/Evaluation/Eval_Dataset_Reduced/Man_target/ \
+	-output_dir=output_wavs/colab_v2/ \
+	-attr=OneShot/models/vctk_colab/VCTK_attr.pkl \
+	-oneshot_conf=OneShot/models/vctk_colab/.config.yaml \
+	-oneshot_model=OneShot/models/vctk_22_kl_1_full_200k.ckpt \
+	-sample_rate=22050 \
+	-data_conf=Waveglow/config.json \
+	-waveglow_path=Waveglow/models/waveglow_256channels_universal_v5.pt \
+	-sigma=0.6\
+	--is_fp16 \
+	-denoiser_strength=0.1
