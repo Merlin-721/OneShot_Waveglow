e903a779126b9aa442bda0fca2e9a37c37500f71
ThorChadMerlin <merlinlindsay@hotmail.com>
2021-09-09 18:23:22 +0100

    demo runtime

--- dual_inference.py
+++ dual_inference.py
@@ -9,6 +9,7 @@ from show_mel import plot_data
 from OneShot.inference import OneShotInferencer
 from Waveglow.inference import WaveglowInferencer
 import time
+from scipy.io.wavfile import write
 
 def files_from_path(path):
 	if os.path.isdir(path):
@@ -28,6 +29,7 @@ if __name__ == '__main__':
 	parser.add_argument('-source', '-s', help='source wav path')
 	parser.add_argument('-target', '-t', help='target wav path')
 	parser.add_argument('-output_dir', '-o', help='output wav path')
+	parser.add_argument('--plot', action='store_true', help='plot mel spectrograms')
 
 	# OneShot
 	parser.add_argument('-attr', '-a', help='data mean & std attr file path')
@@ -62,14 +64,13 @@ if __name__ == '__main__':
 		os.makedirs(args.output_dir)
 
 
-	oneshot_inferencer = OneShotInferencer(config=oneshot_conf, args=args)
+	oneshot_inferencer = OneShotInferencer(config=oneshot_conf, args=args, verbose=False)
 	waveglow_inferencer = WaveglowInferencer(args)
 
 	with open(args.data_conf) as f:
 		data_config = f.read()
 	data_config = json.loads(data_config)["data_config"]
 
-	start = time.time()
 	utt_count = 0
 	for i, speaker in enumerate(np.unique(speakers)):	
 		utts = np.where(np.array(speakers) == speaker)
@@ -80,13 +81,20 @@ if __name__ == '__main__':
 		for source in np.array(wav_fpaths)[utts]:
 			utt_count += 1
 			print(f"\nConverting {source.name} to {target.name}")
-			print("Running OneShot")
-			mel,_ = oneshot_inferencer.inference_from_path(data_config, source, target)
+			mel,os_duration = oneshot_inferencer.inference_from_path(data_config, source, target, plot=args.plot)
+			print(f"OneShot ran in {os_duration:.3f} seconds")
 
-			print("Running Waveglow")
+			start = time.time()
 			name = f"{source.stem}_{target.stem}.wav"	
 			out_path = Path(args.output_dir,speaker,name)	
-			waveglow_inferencer.inference(mel.T, out_path)
-	end = time.time()
-	ET = end - start
-	print(f"\nTotal time for {i+1} speakers and {utt_count} utterances: {ET}")
+			audio = waveglow_inferencer.inference(mel.T, out_path, save_wav=False)
+			wg_duration = time.time() - start
+			print(f"WaveGlow ran in {wg_duration:.3f} seconds")
+			total_duration = os_duration + wg_duration
+			hertz = audio.shape[0] / total_duration
+			print(f"\nSynthesised {audio.shape[0]} samples in {total_duration:.3f} seconds at {hertz/1000:.2f} kHz")
+
+			print(f"\nWriting audio to {out_path}")
+			write(out_path, args.sample_rate, audio)
+
+			print("="*40)
