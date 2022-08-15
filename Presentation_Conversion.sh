d7b54d66dcccbfab1bb4d33448b274cae88a2510
ThorChadMerlin <merlinlindsay@hotmail.com>
2022-08-15 10:19:16 +0100

    presentation

---
+++ Presentation_Conversion.sh
@@ -0,0 +1,12 @@
+python -W ignore dual_inference.py \
+	-source=Presentation_Sources/ \
+	-target=Presentation_Target/p269_023.wav \
+	-output_dir=Presentation_Output \
+	-attr=OneShot/models/vctk_colab/VCTK_attr.pkl \
+	-oneshot_conf=OneShot/models/vctk_colab/.config.yaml \
+	-oneshot_model=OneShot/models/vctk_22_kl_1_full_200k.ckpt \
+	-sample_rate=22050 \
+	-data_conf=Waveglow/config.json \
+	-waveglow_path=Waveglow/models/waveglow_256channels_universal_v5.pt \
+	-sigma=1\
+	-denoiser_strength=0
